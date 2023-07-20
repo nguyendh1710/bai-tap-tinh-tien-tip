493e48ecca59f569e9f66821ee805efad0b625d0
Xuan Phuong <daixuanphuong@gmail.com>
2023-07-20 07:21:20 +0700

    eddit lần 1

--- js/indedx.js
+++ js/indedx.js
@@ -2,8 +2,10 @@ function calculateTip() {
     let xPay = document.getElementById("pay").value;
     let bo = document.getElementById("tip-1").value;
     let total = xPay * bo;
-    let person = document.getElementById("share-1").value.selected;
+    console.log( total);
+    let person = document.getElementById("share-1").selected.value;
     let all = total / person;
+    console.log(all);
     let output = document.getElementById("output-1");
     output.innerHTML = `${"AMOUNT TIP:"}
     ${all}
